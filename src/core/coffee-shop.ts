b5fcefbd76a5a1e1506578f48aaecf13da4bd362
Darius Costolas <dariuscostolas@yahoo.com>
2021-09-15 21:23:23 +0300

    update comment

--- src/core/coffee-shop.ts
+++ src/core/coffee-shop.ts
@@ -5,10 +5,14 @@ import { calculateDistance, roundNumber } from "../utils/utils";
 export class CoffeeShop {
   constructor(public name: string, public location: Location) {}
 
-  /**
+  /** 
+   * REMOVED HAVERSINE CALCULATION BECAUSE THE EARTH IS FLAT
+   * 
    * Calculates the distance between this coffee shop and
    * the location given as argument and returns the distance
    * in KMs
+   * 
+   * /s
    */
   public calculateDistance(target: Location): number {
     return roundNumber(calculateDistance(this.location, target));
